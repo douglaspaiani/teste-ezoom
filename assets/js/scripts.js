38f8e0ec794c84b177589e8d447010d2a859a8df
Douglas Paiani <douglaspaianix@gmail.com>
2024-06-24 16:02:17 -0300

    first commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+    $('.posts').slick({
+        dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ],
+    });
+    $('.categories .list-posts').slick({
+        dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ],
+    });
+    $('.open-menu').click(function(){
+        $('.mobile-menu').fadeIn(900).show();
+    })
+    $('.close-menu').click(function(){
+        $('.mobile-menu').fadeOut(900).hide(900);
+    })
+});
